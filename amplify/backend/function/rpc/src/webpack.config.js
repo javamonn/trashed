c0a038f5bda81daedfb9470382d8d1bd4d695606
Daniel Ramirez <javamonn@gmail.com>
2019-08-24 17:15:58 -0400

    feat: lamdba deployment working

--- amplify/backend/function/rpc/src/webpack.config.js
+++ amplify/backend/function/rpc/src/webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
   mode: process.env.NODE_ENV || 'development',
   entry: [path.resolve(__dirname, './index.js')],
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    libraryTarget: 'umd'
   },
   plugins: [
     new webpack.EnvironmentPlugin([
--- amplify/scripts/build-function.sh
+++ amplify/scripts/build-function.sh
@@ -3,7 +3,7 @@
 set -ex
 
 OWN_PATH=`realpath $0`
-RPC_SRC_DIR=$(readlink -f "$OWN_PATH/../backend/function/rpc/src")
+RPC_SRC_DIR=$(readlink -f $(dirname "$OWN_PATH")/../backend/function/rpc/src)
 
 # Build the RPC app.
 (cd $RPC_SRC_DIR && npm run build)
