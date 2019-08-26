9cee1b79b28169343de0215b979731e2f345f116
Daniel Ramirez <javamonn@gmail.com>
2019-08-25 21:53:42 -0400

    WIP: cognito event handlers

--- packages/dumpster/amplify/scripts/build-function.sh
+++ packages/dumpster/amplify/scripts/build-function.sh
@@ -6,8 +6,10 @@ OWN_PATH=`realpath $0`
 FUNCTION_DIR=`readlink -f "$(dirname $OWN_PATH)/../backend/function/"`
 
 RPC_SRC_DIR="$FUNCTION_DIR/rpc/src"
-PRESIGNUP_SRC_DIR="$FUNCTION_DIR/trashed452c5745PreSignup/src"
+PRE_SIGNUP_SRC_DIR="$FUNCTION_DIR/trashed452c5745PreSignup/src"
+POST_CONFIRMATION_SRC_DIR="$FUNCTION_DIR/trashed452c5745PostConfirmation/src"
 
 # Build each of the functions.
 (cd $RPC_SRC_DIR && npm run build)
-(cd $PRESIGNUP_SRC_DIR && npm run build)
+(cd $PRE_SIGNUP_SRC_DIR && npm run build)
+(cd $POST_CONFIRMATION_SRC_DIR && npm run build)
--- packages/dumpster/amplify/scripts/prebuild-function.sh
+++ packages/dumpster/amplify/scripts/prebuild-function.sh
@@ -3,6 +3,13 @@
 set -ex
 
 OWN_PATH=`realpath $0`
-RPC_SRC_DIR=$(readlink -f "$OWN_PATH/../backend/function/rpc/src")
+FUNCTION_DIR=`readlink -f "$(dirname $OWN_PATH)/../backend/function/"`
 
+RPC_SRC_DIR="$FUNCTION_DIR/rpc/src"
+PRE_SIGNUP_SRC_DIR="$FUNCTION_DIR/trashed452c5745PreSignup/src"
+POST_CONFIRMATION_SRC_DIR="$FUNCTION_DIR/trashed452c5745PostConfirmation/src"
+
+# Install depedencies for each of the functions.
 (cd $RPC_SRC_DIR && npm install)
+(cd $PRE_SIGNUP_SRC_DIR && npm install)
+(cd $POST_CONFIRMATION_SRC_DIR && npm install)
